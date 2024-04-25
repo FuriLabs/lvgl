31c437f7262ee6ec2a027f3c0bb10910b2f93479
David Badiei <davidbadiei123@gmail.com>
2024-04-24 22:41:37 -0400

    got backspace typing and arrows to work

---
+++ src/widgets/keyboard/lv_keyboard_global.h
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2021 Johannes Marbach
+ * Copyright 2024 David Badiei
+ *
+ * This file is part of furios-terminal, hereafter referred to as the program.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef LV_KEYBOARD_GLOBAL_H
+#define LV_KEYBOARD_GLOBAL_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "lvgl/lvgl.h"
+#include "lv_drv_conf.h"
+#include "squeek2lvgl/sq2lv.h"
+
+#define BUFFER_SIZE 4096
+
+char commandBuffer[BUFFER_SIZE];
+extern int commandBufferPos;
+
+#endif /* LV_KEYBOARD_GLOBAL_H */
